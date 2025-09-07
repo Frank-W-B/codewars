9815ce24e8e7939073e684507374efa98931ced2
Frank-W-B <frank.burkholder@gmail.com>
2025-09-06 21:45:31 -0600

    formatting

--- 8kyu/remove_first_last_character.py
+++ 8kyu/remove_first_last_character.py
@@ -1,9 +1,9 @@
 # You are given a string containing a sequence of character sequences separate
 # by commas. Write a function which returns a new string containing the same
 # character sequences except the first and the last ones but this time
-# separated by spaces.  If the input string is empty or the removal of the first
-# and last items would cause the resulting string to be empty, return an empty
-# value (represented as a generic value NULL in the examples below.)
+# separated by spaces.  If the input string is empty or the removal of the
+# first and last items would cause the resulting string to be empty, return an
+# empty value (represented as a generic value NULL in the examples below.)
 
 # Examples
 # "1,2,3"      =>  "2"
@@ -21,6 +21,7 @@ def remove_first_and_last_char(string: str) -> str:
         subset = ' '.join(vals[1:-1])
         return subset
     return None
+
 
 # best practice solution
 def array(strng):
